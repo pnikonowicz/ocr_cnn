04f515f7df4b403ab5e6ccc792d852f373326b8e
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-03-13 16:28:55 -0400

    read all of the files in dataset

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+func log(message string) {
+	fmt.Println(message)
+}
 
 func main() {
-	fmt.Println("hello")
+	wd, _ := os.Getwd()
+	dataset_dir := path.Join(wd, "dataset")
+
+	for number := range []int{0,1,2,3,4,5,6,7,8,9} {
+
+		dir := path.Join(dataset_dir, string(rune('0' + number)))
+		log(dir)
+		dir_iterator, err := os.ReadDir(dir)
+
+		if(err != nil) {
+			log("could not read dir")
+			os.Exit(1)
+		}
+
+		for _, file := range dir_iterator {
+			fmt.Println(file, file.IsDir())
+		}
+	}
+
 }
