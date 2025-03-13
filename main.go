7a52ac5cee8d34b565709137716e11a391589d1f
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-03-13 16:51:55 -0400

    assert resolution is the same

--- main.go
+++ main.go
@@ -21,11 +21,15 @@ func main() {
 	wd, _ := os.Getwd()
 	dataset_dir := path.Join(wd, "dataset")
 
+	expected_width := 64
+	expected_height := 64
+
 	for number := range []int{0,1,2,3,4,5,6,7,8,9} {
 
 		dir := path.Join(dataset_dir, string(rune('0' + number)))
-		log(dir)
 		dir_iterator, err := os.ReadDir(dir)
+
+		log(fmt.Sprintf("checking dir: %s", dir))
 
 		if(err != nil) {
 			printAndTerminate("could not read dir")
@@ -47,8 +51,11 @@ func main() {
 			width := bounds.Max.X - bounds.Min.X
 			height := bounds.Max.Y - bounds.Min.Y
 
-			log(fmt.Sprintf("w: %d h: %d", width, height))
+			if width != expected_width || height != expected_height {
+				printAndTerminate(fmt.Sprintf("bad image: w: %d h: %d", width, height))
+			}
 		}
 	}
 
+	log("all images have same resolution")
 }
