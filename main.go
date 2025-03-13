9bda87a28ba1d73c8014acaab2b1ca6895fb93d1
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-03-13 16:47:55 -0400

    print file dimensions

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"image/png"
 	"os"
 	"path"
 )
 
 func log(message string) {
 	fmt.Println(message)
+}
+
+func printAndTerminate(message string) {
+	log(message)
+	os.Exit(1)
 }
 
 func main() {
@@ -21,12 +28,26 @@ func main() {
 		dir_iterator, err := os.ReadDir(dir)
 
 		if(err != nil) {
-			log("could not read dir")
-			os.Exit(1)
+			printAndTerminate("could not read dir")
 		}
 
-		for _, file := range dir_iterator {
-			fmt.Println(file, file.IsDir())
+		for _, file_entry := range dir_iterator {
+			file_name := path.Join(dir, file_entry.Name())
+			file_contents, err := os.ReadFile(file_name)
+			if err != nil {
+				printAndTerminate(fmt.Sprintf("could not read file: %s", file_name))
+			}
+
+			img, err := png.Decode(bytes.NewReader(file_contents))
+			if err != nil {
+				printAndTerminate(fmt.Sprintf("could not read png: %s", file_name))
+			}
+
+			bounds := img.Bounds()
+			width := bounds.Max.X - bounds.Min.X
+			height := bounds.Max.Y - bounds.Min.Y
+
+			log(fmt.Sprintf("w: %d h: %d", width, height))
 		}
 	}
 
