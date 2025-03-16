70840ccc45847c02d339a076e11982296fb5f8dc
Paul Nikonowicz <paul.nikonowicz@gmail.com>
2025-03-16 12:17:58 -0400

    train: use actual png resolution

--- cmd/train/main.go
+++ cmd/train/main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"image/png"
 	"math"
 	"ocr_cnn/pkg/neuron"
+	"os"
+	"path"
 )
 
-func findResolution() (int,int) {
-	return 32,32
+func log(message string) {
+	fmt.Println(message)
+}
+
+func printAndTerminate(message string) {
+	log(message)
+	os.Exit(1)
+}
+
+func findResolution(dataset_dir string) (int, int) {
+	file_path := path.Join(dataset_dir, "0", "Abadi_0.png")
+	file_contents, err := os.ReadFile(file_path)
+	if err != nil {
+		printAndTerminate(fmt.Sprintf("could not read file: %s", file_path))
+	}
+	img, err := png.Decode(bytes.NewReader(file_contents))
+	if err != nil {
+		printAndTerminate(fmt.Sprintf("could not read png: %s", file_path))
+	}
+
+	bounds := img.Bounds()
+
+	x := bounds.Max.X - bounds.Min.X
+	y := bounds.Max.Y - bounds.Min.Y
+
+	return x, y
 }
 
 func main() {
-	resolutionX, resolutionY := findResolution()
+	wd, _ := os.Getwd()
+	dataset_dir := path.Join(wd, "translated_dataset")
+
+	resolutionX, resolutionY := findResolution(dataset_dir)
 	layerSize := resolutionX * resolutionY
 	const numberOfHiddenLayers = 2
 
@@ -19,20 +50,20 @@ func main() {
 	var hiddenLayers [][]neuron.Neuron
 	var outputLayer []neuron.Neuron
 
-	for range(layerSize) {
+	for range layerSize {
 		inputLayer = append(inputLayer, neuron.Neuron{})
 	}
 
 	for i := 1; i <= numberOfHiddenLayers; i++ {
 		reductionDivisior := int(math.Pow(2, float64(i)))
 		var hiddenLayer []neuron.Neuron
-		for range(len(inputLayer) / reductionDivisior) {
+		for range len(inputLayer) / reductionDivisior {
 			hiddenLayer = append(hiddenLayer, neuron.Neuron{})
 		}
 		hiddenLayers = append(hiddenLayers, hiddenLayer)
 	}
 
-	for i := 0; i<10; i++ {
+	for i := 0; i < 10; i++ {
 		outputLayer = append(outputLayer, neuron.Neuron{})
 	}
 
